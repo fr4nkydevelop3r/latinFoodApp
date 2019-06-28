e1a11ce74f59a1f14067d887ed7076c19e67e23c
Frank Andrade <fr4nky.develop3r@gmail.com>
2019-06-28 11:59:18 -0400

    Updates

---
+++ precache-manifest.036d247e14f00a120439ba291119fd34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a524b8b69ed3f7b6bc60fc2b43b43325",
+    "url": "/latinFoodApp/index.html"
+  },
+  {
+    "revision": "da3422b31aa1132c50cb",
+    "url": "/latinFoodApp/static/css/main.a90c25ab.chunk.css"
+  },
+  {
+    "revision": "40411b9cbfea384615c8",
+    "url": "/latinFoodApp/static/js/2.6b1cc622.chunk.js"
+  },
+  {
+    "revision": "da3422b31aa1132c50cb",
+    "url": "/latinFoodApp/static/js/main.4de23c12.chunk.js"
+  },
+  {
+    "revision": "3694b6017cab6454d756",
+    "url": "/latinFoodApp/static/js/runtime~main.4cd6ca4d.js"
+  }
+]);
